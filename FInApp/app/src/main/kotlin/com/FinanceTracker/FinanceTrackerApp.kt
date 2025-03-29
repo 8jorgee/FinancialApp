3e3f6bd2c2ff4ac9bfc143e6cd7d6cdee9a7dc32
8jorgee <jormarove@usal.es>
2025-03-29 14:15:55 +0100

    update on FinancialTrackerApp.kt

--- FInApp/app/src/main/kotlin/com/FinanceTracker/FinanceTrackerApp.kt
+++ FInApp/app/src/main/kotlin/com/FinanceTracker/FinanceTrackerApp.kt
@@ -1,26 +1,65 @@
-package com.financetracker.ui
+package com.financetracker
 
-import android.os.Bundle
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import android.content.Context
+import androidx.room.Room
+import com.google.firebase.FirebaseApp
+import com.financetracker.data.local.FinanceDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
+import com.financetracker.data.repository.AuthRepository
+import com.financetracker.data.repository.FinanceRepository
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.financetracker.ui.auth.LoginScreen
-import com.financetracker.ui.auth.RegisterScreen
-import com.financetracker.ui.main.DashboardScreen
+import androidx.navigation.compose.rememberNavController
+import com.financetracker.ui.navigation.AppNavigation
 import com.financetracker.ui.theme.FinanceTrackerTheme
+
+@HiltAndroidApp
+class FinanceTrackerApp : Application() {
+    companion object {
+        lateinit var instance: FinanceTrackerApp
+            private set
+        lateinit var database: FinanceDatabase
+            private set
+        lateinit var auth: FirebaseAuth
+            private set
+        lateinit var firestore: FirebaseFirestore
+            private set
+        lateinit var messaging: FirebaseMessaging
+            private set
+        lateinit var authRepository: AuthRepository
+            private set
+        lateinit var financeRepository: FinanceRepository
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        FirebaseApp.initializeApp(this)
+        database = Room.databaseBuilder(
+            applicationContext,
+            FinanceDatabase::class.java,
+            "finance_db"
+        ).build()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        messaging = FirebaseMessaging.getInstance()
+        authRepository = AuthRepository(auth, firestore)
+        financeRepository = FinanceRepository(database.financeDao())
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FinanceTrackerTheme {
-                // A surface container using the 'background' color from the theme
                 MainScreen()
             }
         }
@@ -30,18 +69,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "login") {
-        composable("login") {
-            LoginScreen(navController = navController)
-        }
-        composable("register") {
-            RegisterScreen(navController = navController)
-        }
-        composable("dashboard") {
-            DashboardScreen(navController = navController)
-        }
-    }
+    AppNavigation(navController)
 }
 
 @Preview(showBackground = true)
